2e2ec2e584678657f98d287d84e6e72de84c6e9c
aslamosama <90507714+aslamosama@users.noreply.github.com>
2025-06-05 12:46:07 +0500

    fix repo paths

--- 03_postchroot/15_my_programs.sh
+++ 03_postchroot/15_my_programs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-cd ~/.config/lf/lf-file-handler || exit 1
+cd ~/.local/src/lf-file-handler || exit 1
 bear -- make
 sudo make install
 cd
 
-cd ~/.config/fetch || exit 1
+cd ~/.local/src/fetch || exit 1
 sudo make install
 cd
 
