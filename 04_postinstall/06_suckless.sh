bf56b95d7a9a62587bcf3b2eb56b669b8c358577
aslamosama <90507714+aslamosama@users.noreply.github.com>
2025-06-27 14:35:56 +0500

    add aria2p

--- 04_postinstall/06_suckless.sh
+++ 04_postinstall/06_suckless.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
 for program in dmenu dwm dwmblocks st slock; do
-  cd ~/.config/suckless/$program && lazygit && sudo make install
+  cd ~/.config/suckless/$program
+  git config merge.ours.driver true
+  lazygit
+  sudo make install
 done
--- 04_postinstall/12_other_packages.sh
+++ 04_postinstall/12_other_packages.sh
@@ -5,3 +5,4 @@ npm install -g live-server
 
 # Install all pipx packages
 pipx install git+https://github.com/veneres/py-pandoc-include-code.git
+pipx install aria2p[tui]
